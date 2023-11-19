b85429840827de3dbe73fced577bb582bc5705b9
Shay Dubrovsky <shaydu9@gmail.com>
2023-11-19 18:30:33 +0200

    Initial Commit

---
+++ MobileNetTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileNetTest
+//
+//  Created by Shay Dubrovsky on 19/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
